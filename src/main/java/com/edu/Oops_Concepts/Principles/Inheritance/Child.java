8fc8ace0990e7d3d547ee4bb5fa9d77b607f56ab
Pranaykr <pronoy.bose07@gamil.com>
2025-03-15 20:59:07 +0530

    Added Files

---
+++ src/main/java/com/edu/Oops_Concepts/Principles/Inheritance/Child.java
@@ -0,0 +1,38 @@
+package com.edu.Oops_Concepts.Principles.Inheritance;
+
+public class Child extends Parent{
+    double weight;
+
+    public Child(double weight) {
+        this.weight = -1;
+    }
+
+    public Child(double length, double width, double height, double weight) {
+        super(length, width, height);
+        this.weight = weight;
+    }
+
+    public void information() {
+        String str = Double.toString(weight);
+        super.information(str);
+    }
+
+
+    // This is a Copy Constructor
+    // Here, when we write super(child), we access the copy constructor of Parent class
+    // And We don't get an error as because Child class has access to all the Members of Parent for obvious reasons
+    public Child(Child child) {
+        super(child);
+        weight = child.weight;
+    }
+
+    /**
+     * public ParentWeight(){
+     *         this.length = 10;
+     *     }
+     *
+     * for the above example, length cannot be accessed as its declared Private
+     * Members of a class those are declared as Private cannot be accessed by child class
+     */
+
+}
