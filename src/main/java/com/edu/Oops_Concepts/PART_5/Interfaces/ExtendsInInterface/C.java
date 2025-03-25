a1a5a11b482d229d89adca3eb335924ed29602a0
Pranaykr <pronoy.bose07@gamil.com>
2025-03-25 09:43:43 +0530

    Updated Code

---
+++ src/main/java/com/edu/Oops_Concepts/PART_5/Interfaces/ExtendsInInterface/C.java
@@ -0,0 +1,13 @@
+package com.edu.Oops_Concepts.PART_5.Interfaces.ExtendsInInterface;
+
+public class C implements B {
+    @Override
+    public void greet() {
+
+    }
+
+    @Override
+    public void fun() {
+
+    }
+}
