a098520921df86de14eb8855bb704f54c20890c7
PranayKr2015 <pranaykr2015@PranayKr2015s-MacBook-Pro.local>
2026-01-10 17:12:41 +0530

    Updated/deleted files

---
+++ src/main/java/com/edu/LeetCodeProblems/LongestSubStringWithoutRepeatingCharacters.java
@@ -0,0 +1,34 @@
+package com.edu.LeetCodeProblems;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubStringWithoutRepeatingCharacters {
+    public static void main(String[] args) {
+        solution solution = new solution();
+        int sol = solution.lengthOfLongestSubstring("abcabcbb");
+        System.out.println(sol);
+    }
+}
+
+class solution {
+    public int lengthOfLongestSubstring(String s) {
+
+        int left = 0;
+        int maxLength = 0;
+        Set<Character> characterSet = new HashSet<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            while (characterSet.contains(c)) {
+                characterSet.remove(s.charAt(left));
+                left++;
+            }
+
+            characterSet.add(c);
+            maxLength = Math.max(maxLength, i - left +1);
+        }
+        return maxLength;
+    }
+}
